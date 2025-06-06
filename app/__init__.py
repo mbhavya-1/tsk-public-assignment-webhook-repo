633aea1690840c46d4b0024fe2cae3ea95c4ed91
Bhavya Mehta <mehta.7@iitj.ac.in>
2025-06-06 16:10:00 +0530

    Extracting Pull Requests

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,21 @@ from flask import Flask
 
 from app.webhook.routes import webhook
 
+from flask_cors import CORS
+from dotenv import load_dotenv
 
+load_dotenv()
 # Creating our flask app
 def create_app():
 
     app = Flask(__name__)
     
     # registering all the blueprints
+    CORS(app)
     app.register_blueprint(webhook)
+
+    @app.route('/')
+    def hello_world():
+        return "<p>Hello, World!</p>"
     
     return app
--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -1,7 +1,32 @@
-from flask import Blueprint, json, request
+from flask import Blueprint, json, request, jsonify
+from .db import collection
 
 webhook = Blueprint('Webhook', __name__, url_prefix='/webhook')
 
 @webhook.route('/receiver', methods=["POST"])
 def receiver():
-    return {}, 200
+    if request.headers.get('Content-Type') == 'application/json':
+        payload = request.json
+        event_type = request.headers.get('X-GitHub-Event')  # to verify it's a PR event
+
+        if event_type == 'pull_request':
+            pr = payload.get("pull_request", {})
+            data = {
+                "request_id": str(pr.get("id")),
+                "author": pr.get("user", {}).get("login"),
+                "action": "PULL REQUEST",
+                "from_branch": pr.get("head", {}).get("ref"),
+                "to_branch": pr.get("base", {}).get("ref"),
+                "timestamp": pr.get("updated_at")  # already in ISO format
+            }
+            collection.insert_one(data),
+            return jsonify({"message": "Pull Request data saved"}), 200
+
+        return jsonify({"message": "Not a pull request event"}), 200
+
+    return jsonify({"error": "Invalid Content-Type"}), 400
+
+@webhook.route('/get-updates', methods=["GET"])
+def get_updates():
+    events = list(collection.find({}, {"_id": 0}))  # exclude MongoDB's default _id
+    return jsonify(events)
